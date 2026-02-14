9e2eede3fc8b056240776f747d5b165381219a4c
Edson J. Vargas <edson.jhair@yahoo.com>
2026-02-14 17:37:37 -0500

    Auth screens

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
 	endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
 	platform: 'com.jsm.foodordering',
 	projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+	databaseId: '6990dae200014b17d054',
+	userCollectionId: 'user',
 };
