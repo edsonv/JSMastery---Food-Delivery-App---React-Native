34cab697e0e9fa41fb76191d0f94e75f77bf4556
Edson J. Vargas <edson.jhair@yahoo.com>
2026-02-15 22:34:00 -0500

    Setup zustand auth store

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,34 @@
+import useAuthStore from '@/store/auth.store';
+import * as Sentry from '@sentry/react-native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import './global.css';
-import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
-  dsn: 'https://5f16dde2274969c56f3b848463d4b49f@o4508896855982080.ingest.us.sentry.io/4510893194870784',
+	dsn: 'https://5f16dde2274969c56f3b848463d4b49f@o4508896855982080.ingest.us.sentry.io/4510893194870784',
 
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
+	// Adds more context data to events (IP address, cookies, user, etc.)
+	// For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+	sendDefaultPii: true,
 
-  // Enable Logs
-  enableLogs: true,
+	// Enable Logs
+	enableLogs: true,
 
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+	// Configure Session Replay
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1,
+	integrations: [
+		Sentry.mobileReplayIntegration(),
+		Sentry.feedbackIntegration(),
+	],
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: __DEV__,
 });
 
 export default Sentry.wrap(function RootLayout() {
+	const { isLoading, fetchAuthenticatedUser } = useAuthStore();
 	const [fontsLoaded, error] = useFonts({
 		'QuickSand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
 		'QuickSand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -36,6 +41,12 @@ export default Sentry.wrap(function RootLayout() {
 		if (error) throw error;
 		if (fontsLoaded) SplashScreen.hideAsync();
 	}, [fontsLoaded, error]);
+
+	useEffect(() => {
+		fetchAuthenticatedUser();
+	}, []);
+
+	if (!fontsLoaded || isLoading) return null;
 
 	return <Stack screenOptions={{ headerShown: false }} />;
 });
